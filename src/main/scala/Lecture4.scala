540af0c961319f6328c806fcc3b008d09a784b13
cubazis <grebenkin@phystech.edu>
2018-02-05 17:32:48 +0300

    Lecture4 support code

---
+++ src/main/scala/Lecture4.scala
@@ -0,0 +1,16 @@
+/** import area */
+
+object Lecture4 {
+        def main(args: Array[String]): Unit = {
+                println("This is the fourth lecture support code")
+                println("You can run all the examples of 'repl' in REPL shell")
+                repl()
+        }
+
+        def repl(unit: Unit): Unit = {
+                /** Companion Objects */
+                println("Companion Objects")
+
+        }
+}
+
